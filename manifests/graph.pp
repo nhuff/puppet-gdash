37a792271c68cf5f09adeb74de9644f1744799fe
Nathan Huff <nrhuff@umn.edu>
2012-11-27 15:24:46 -0600

    Add a requirement to create a dashboard before the graphs in it

--- manifests/graph.pp
+++ manifests/graph.pp
@@ -10,12 +10,14 @@ define gdash::graph(
   include gdash::params
   $gfile = "${gdash::params::dashboard_dir}/${dashboard_group}/${dashboard}/${title}.graph"
   concat{$gfile:
-    owner => 'root',
-    group => 'root',
-    mode  => '0644',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => Gdash::Dashboard[$dashboard],
   }
   concat::fragment{"${title}_header":
     target  => $gfile,
     content => template('gdash/graph_header.erb'),
+    order   => '01',
   }
 }
