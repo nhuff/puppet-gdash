8c010dee4d59f1de84b5921371ca6b5bce999025
Nathan Huff <nrhuff@umn.edu>
2012-11-30 10:07:01 -0600

    Cleanup dashboards and graphs that don't exist any longer

--- manifests/dashboard_group.pp
+++ manifests/dashboard_group.pp
@@ -1,9 +1,12 @@
 define gdash::dashboard_group{
   include gdash::params
   file{"${gdash::params::dashboard_dir}/${title}":
-    ensure => 'directory',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
+    ensure  => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    purge   => true,
+    force   => true,
+    recurse => true,
   }
 }
